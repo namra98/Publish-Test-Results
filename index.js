946a471e80a31be6617805fd66b3d5c5dc39ec3c
Namra Prajapati <nbrprajapati@gmail.com>
2020-05-23 20:33:05 +0530

    Send Event to App

--- index.js
+++ index.js
@@ -18,18 +18,43 @@ function getTestRun(secret) {
       });
 }
 
-function sendDispatchEvent(token) {
-  const octokit = new github.GitHub(token);
+function createCheck() {
+  const headers = {
+    'Content-Type': 'application/json',
+    Accept: 'application/vnd.github.antiope-preview+json',
+    Authorization: `Bearer ${GITHUB_TOKEN}`,
+    'User-Agent': 'PTR-action'
+  }
 
-  var fullrepo = github.context.repo;
-  var repo = fullrepo.repo;
-  var owner = fullrepo.owner;
-  octokit.repos.createDispatchEvent({
-    owner,
-    repo,
-    event_type: 'test'
-  });
+  const body = {
+    name: 'publishtest',
+    head_sha: GITHUB_SHA,
+    status: 'in_progress',
+    started_at: new Date()
+  }
+
+  const { data } = request(`https://api.github.com/repos/${github.context.repo.owner}/${github.context.repo.repo}/check-runs`, {
+    method: 'POST',
+    headers,
+    body
+  })
+
+  console.log(`Created Check Run : ${data}`);
+
 }
+
+// function sendDispatchEvent(token) {
+//   const octokit = new github.GitHub(token);
+
+//   var fullrepo = github.context.repo;
+//   var repo = fullrepo.repo;
+//   var owner = fullrepo.owner;
+//   octokit.repos.createDispatchEvent({
+//     owner,
+//     repo,
+//     event_type: 'test'
+//   });
+// }
 
 async function run() {
   try {
@@ -45,11 +70,17 @@ async function run() {
     console.log(github.context.payload);
     console.log(github.context.workflow);
 
-    var dispatchResponse = sendDispatchEvent(githubToken);
-    console.log(`REPO_DISPATCH : \n ${dispatchResponse}`)
+    // var dispatchResponse = sendDispatchEvent(githubToken);
+    // console.log(`REPO_DISPATCH : \n ${dispatchResponse}`)
 
+    // Get Test Run using Token.
     var testRun = getTestRun(secret);
-    console.log(`PTR Secret\n ${testRun}`)
+    console.log(`Test Run\n ${testRun}`);
+
+    // Send run info to GitHub App.
+    var check = createCheck();
+  
+
   } catch (error) {
     core.setFailed(error.message);
   }
