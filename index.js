71865ef5dcd92382e6675cd6508004169c71db5b
Namra Prajapati <nbrprajapati@gmail.com>
2020-06-01 14:56:52 +0530

    Parsing

--- index.js
+++ index.js
@@ -1,73 +1,191 @@
 const core = require('@actions/core')
 const github = require('@actions/github')
-var request = require('request-promise');
+const request = require('request-promise');
+const xmljs = require('xml-js');
+const fs = require('fs');
 
-
-function getTestRun(secret) {
-    var options = {
-        'method': 'GET',    
-        'url': `https://tcman.codedev.ms/${github.context.repo.repo}/_apis/test/runs/1`,
-        'headers': {
-          'Authorization': 'Bearer ' + secret,
-        }
-      };
-      request(options, function (error, response) { 
-        if (error) throw new Error(error);
-        console.log(response.body);
-        return response.body;
-      });
+class TestRun {
+  constructor(RunId, State, Title, LastUpdated, Owner, IncompleteTests, IterationId, DropLocation, ErrorMessage, StartDate, CompleteDate, Controller, PostProcessState, Revision, LastUpdatedBy, Type, TestEnvironmentId, Version, Comment, TotalTests, PassedTests, NotApplicableTests, UnanalyzedTests, CreationDate) {
+    this.RunId = RunId;
+    this.state = State;
+    this.Title = Title;
+    this.LastUpdated = LastUpdated;
+    this.Owner = Owner;
+    this.IncompleteTests = IncompleteTests;
+    this.IterationId = IterationId;
+    this.DropLocation = DropLocation;
+    this.ErrorMessage = ErrorMessage;
+    this.StartDate = StartDate;
+    this.CompleteDate = CompleteDate;
+    this.Controller = Controller;
+    this.PostProcessState = PostProcessState;
+    this.Revision = Revision;
+    this.LastUpdatedBy = LastUpdatedBy;
+    this.Type = Type;
+    this.TestEnvironmentId = TestEnvironmentId;
+    this.Version = Version;
+    this.Comment = Comment;
+    this.TotalTests = TotalTests;
+    this.PassedTests = PassedTests;
+    this.NotApplicableTests = NotApplicableTests;
+    this.UnanalyzedTests = UnanalyzedTests;
+    this.CreationDate = CreationDate;
+  }
 }
 
-function publishTestRuns(secret, check_run_id) {
-  var testRun = {
-    TestRunId : "2",
-    RunId : check_run_id,
-    State : "0",
-    Title : "GitHub Action",
-    LastUpdated : "2019-07-01T04-00-00.000Z",
-    Owner : "GitHub Action.",
-    IncompleteTests : "0",
-    IterationId : "23",
-    DropLocation : "droploc",
-    ErrorMessage : "Error Message",
-    StartDate : "2018-08-26T12:58:34.860",
-    CompleteDate : "2018-08-26T12:58:34.860",
-    Controller : "controller",
-    PostProcessState : "0",
-    Revision : "3",
-    LastUpdatedBy : "85c65d89-072a-4923-8360-497a4053153c",
-    Type : "0",
-    TestEnvironmentId : "85c65d89-072a-4923-8360-497a4053153c",
-    Version : "1",
-    Comment : "No Comments.",
-    TotalTests : "10",
-    PassedTests : "9",
-    NotApplicableTests : "0",
-    UnanalyzedTests : "1",
-    CreationDate : "2018-08-26T12:58:34.860"
-  } 
 
-  console.log(secret);
-  console.log(github.context.repo.repo);
-  request({
-    url: `https://tcman.codedev.ms/${github.context.repo.repo}/_apis/test/runs?api-version=1.0`,
-    method: "POST",
-    headers: {
-      'Authorization':'Bearer ' + secret,
-    },
-    json: true,
-    body: testRun
-  }, function(error, response, body) {
-    if (!error && response.statusCode == 200) {
-      console.log("No Error", body);
-      return body;
-    }
-    else {
-      console.log("Error", body);
-      return body;
+class TestResult {
+  constructor(TestRunId, TestCaseRefId, TestResultId, TestCaseId, CreationDate, LastUpdated, LastUpdatedBy, LastUpdateBy, Outcome, State, Revision, DateStarted, DateCompleted, Owner, RunBy, Duration, ComputerName, FailureType, ResolutionStateId, ResetCount, Comment, StackTrace, ErrorMessage, AfnStripId, RV, AutomatedTestName) {
+    this.TestRunId = TestRunId;
+    this.TestCaseRefId = TestCaseRefId;
+    this.TestResultId = TestResultId;
+    this.TestCaseId = TestCaseId;
+    this.CreationDate = CreationDate;
+    this.LastUpdated = LastUpdated;
+    this.LastUpdatedBy = LastUpdatedBy;
+    this.LastUpdateBy = LastUpdateBy;
+    this.Outcome = Outcome;
+    this.State = State;
+    this.Revision = Revision;
+    this.DateStarted = DateStarted;
+    this.DateCompleted = DateCompleted;
+    this.Owner = Owner;
+    this.RunBy = RunBy;
+    this.Duration = Duration;
+    this.ComputerName = ComputerName;
+    this.FailureType = FailureType;
+    this.ResolutionStateId = ResolutionStateId;
+    this.ResetCount = ResetCount;
+    this.Comment = Comment;
+    this.StackTrace = StackTrace;
+    this.ErrorMessage = ErrorMessage;
+    this.AfnStripId = AfnStripId;
+    this.RV = RV;
+    this.AutomatedTestName = AutomatedTestName;
+  }
+}
+
+
+function Publish(filepath, secret, check_run_id) {
+
+  const xml = fs.readFileSync(filepath, 'utf8');
+  const jsonData = JSON.parse(convert.xml2json(xml, { compact: true, spaces: 2 }));
+
+  for (var obj in jsonData) {
+    if (obj == 'TestRun') {
+      // Summary of TestRun.
+      var counters = jsonData[obj]['ResultSummary']['Counters']['_attributes'];
+
+      // Create Test run object and send it to Tcm.
+      var testRun = new TestRun(
+        "1",
+        check_run_id,
+        jsonData[obj]['TestSettings']['_attributes']['name'],
+        "2019-07-01T04-00-00.000Z",
+        jsonData[obj]['_attributes']['runUser'],
+        "0",
+        "23",
+        "any",
+        "errormessage",
+        jsonData[obj]['Times']['_attributes']['start'],
+        jsonData[obj]['Times']['_attributes']['finish'],
+        "controller",
+        "0",
+        "1",
+        "85c65d89-072a-4923-8360-497a4053153c",
+        "0",
+        "85c65d89-072a-4923-8360-497a4053153c",
+        "1",
+        "comments",
+        counters['total'],
+        counters['passed'],
+        "0",
+        counters["total"] - counters["executed"],
+        jsonData[obj]['Times']['_attributes']['creation']
+      );
+
+      // send it to Tcm and save TestRunId.
+      var testRunResponse = await request({
+        url: `https://tcman.codedev.ms/${github.context.repo.repo}/_apis/test/runs?api-version=1.0`,
+        method: "POST",
+        headers: {
+          'Authorization': 'Bearer ' + secret,
+        },
+        json: true,
+        body: testRun
+      }, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+          console.log("No Error", body);
+          return body;
+        }
+        else {
+          console.log("Error", body);
+          return body;
+        }
+      }
+      );
+
+      console.log(testRunResponse);
+
+      var testRunId = "1";
+
+      // console.log(jsonData[obj]['Results']);
+      for (var result in jsonData[obj]['Results']['UnitTestResult']) {
+
+        // Create Test Results.
+        var testResult = new TestResult(testRunId,
+          "1",
+          "1",
+          "1",
+          jsonData[obj]['Results']['UnitTestResult'][result]['_attributes']['startTime'],
+          jsonData[obj]['Results']['UnitTestResult'][result]['_attributes']['startTime'],
+          'e683309b-8302-411a-92ec-abeff69258a2',
+          "e683309b-8302-411a-92ec-abeff69258a2",
+          jsonData[obj]['Results']['UnitTestResult'][result]['_attributes']['outcome'] === 'Passed' ? "0" : "1",
+          "1",
+          "1",
+          jsonData[obj]['Results']['UnitTestResult'][result]['_attributes']['startTime'],
+          jsonData[obj]['Results']['UnitTestResult'][result]['_attributes']['endTime'],
+          'e683309b-8302-411a-92ec-abeff69258a2',
+          "e683309b-8302-411a-92ec-abeff69258a2",
+          jsonData[obj]['Results']['UnitTestResult'][result]['_attributes']['duration'],
+          jsonData[obj]['TestSettings']['_attributes']['name'],
+          "0",
+          "0",
+          "0",
+          "No Comments.",
+          jsonData[obj]['Results']['UnitTestResult'][result]['_attributes']['outcome'] === 'Failed' ? jsonData[obj]['Results']['UnitTestResult'][result]['Output']['ErrorInfo']['StackTrace']['_text'] : "",
+          jsonData[obj]['Results']['UnitTestResult'][result]['_attributes']['outcome'] === 'Failed' ? jsonData[obj]['Results']['UnitTestResult'][result]['Output']['ErrorInfo']['Message']['_text'] : "",
+          "0",
+          "0",
+          jsonData[obj]['Results']['UnitTestResult'][result]['_attributes']['testName']
+        );
+
+        // Publish Test result.
+        var testResultResponse = await request({
+          url: `https://tcman.codedev.ms/${github.context.repo.repo}/_apis/test/runs/#${testRunId}/results?api-version=1.0`,
+          method: "POST",
+          headers: {
+            'Authorization': 'Bearer ' + secret,
+          },
+          json: true,
+          body: testResult
+        }, function (error, response, body) {
+          if (!error && response.statusCode == 200) {
+            console.log("No Error", body);
+            return body;
+          }
+          else {
+            console.log("Error", body);
+            return body;
+          }
+        }
+        );
+
+        console.log(testResultResponse);
+      }
     }
   }
-  );
 }
 
 async function getCheckRunId(octokit) {
@@ -95,7 +213,7 @@ async function getTcmToken() {
     method: "POST",
     json: true,
     body: body
-  }, function(error, response, body) {
+  }, function (error, response, body) {
     if (!error && response.statusCode == 200) {
       console.log("done", body);
       return body;
@@ -106,20 +224,24 @@ async function getTcmToken() {
     }
   }
   );
-  
+
   console.log(req);
 
   return req;
 }
 
+async function parseXML(filepath) {
+  const testxml = fs.readFileSync(filepath, 'utf8');
+  const jsonData = JSON.parse(xmljs.xml2json(testxml, { compact: true, spaces: 2 }));
+  console.log(jsonData);
+
+}
+
 async function run() {
   try {
     // Read inputs
-    const nameToGreet = core.getInput('who-to-greet');
     var githubToken = core.getInput('github-secret');
-    const secret = core.getInput('ptr-secret');
-    const githubPat = core.getInput('github-pat');
-
+    var filepath = core.getInput('filepath');
     // Get token for Org bt calling GitHub App.
     const TcmToken = await getTcmToken();
     console.log(TcmToken);
@@ -130,7 +252,7 @@ async function run() {
     var check_run_id = await getCheckRunId(octokit);
 
     // Get Test Run using Token.
-    var testRun = publishTestRuns(TcmToken, check_run_id);
+    var testRun = Publish(filepath, TcmToken, check_run_id);
     console.log(`Test Run ${testRun}`);
 
   } catch (error) {
